f23c805e5c1d3f69d5edec67e76c6ef8f5cb8fa1
nthahaianh <ntha.haianh@gmail.com>
2021-09-08 17:25:35 +0700

    commit v1-update

--- app/src/main/java/com/example/tradix/MainActivity.kt
+++ app/src/main/java/com/example/tradix/MainActivity.kt
@@ -16,13 +16,30 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navHostFragment: NavHostFragment
+    lateinit var controller: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        main_bottomNavigationView.setupWithNavController(navController)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        controller = navHostFragment.navController
+        main_bottomNavigationView.setupWithNavController(controller)
     }
 
+    override fun onBackPressed() {
+        if (navHostFragment.childFragmentManager.backStackEntryCount == 0) {
+            val dialog = AlertDialog.Builder(this)
+            dialog.setTitle("Are you sure?")
+                .setMessage("Do you want to exit app?")
+                .setNegativeButton("No") { _: DialogInterface, _: Int -> }
+                .setPositiveButton("Yes") { _: DialogInterface, _: Int ->
+                    finish()
+                }
+                .show()
+        } else {
+            super.onBackPressed()
+        }
+    }
 
 }
